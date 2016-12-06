a0455959082dc8a79d08c6dc632eabcf22db5f4a
Nils Bartels <nbartels@jimgordon.fritz.box>
2016-12-06 21:24:29 +0100

    [ADD] refresh token; [CHANGE] cleanup;

--- lib/cafmal/auth.rb
+++ lib/cafmal/auth.rb
@@ -38,7 +38,7 @@ module Cafmal
         @decoded_token['header'] = JSON.parse(Base64.decode64(@token.split('.')[0]))
         @decoded_token['payload'] = JSON.parse(Base64.decode64(@token.split('.')[1]))
 
-        if (@decoded_token['payload']['role'] == 'user' || @decoded_token['payload']['role'] == 'admin')
+        if (@decoded_token['payload']['role'] != 'worker' && @decoded_token['payload']['role'] != 'alerter')
           team_id = JSON.parse(Cafmal::User.new(@cafmal_api_url, @token).show(@decoded_token['payload']['sub']))["team_id"]
           event = Cafmal::Event.new(@cafmal_api_url, @token)
           event.create({name: 'user.login', message: "#{email} has logged in.", kind: 'login', severity: 'info', team_id: team_id})
@@ -52,31 +52,52 @@ module Cafmal
 
     # we supply the token here, so web does not have to cache the auth obj
     def logout(token)
-      unless token.nil?
-        headers = {"Content-Type" => "application/json", "Authorization" => "Bearer #{token}"}
+      headers = {"Content-Type" => "application/json", "Authorization" => "Bearer #{token}"}
 
-        decoded_token = {}
-        decoded_token['header'] = JSON.parse(Base64.decode64(token.split('.')[0]))
-        decoded_token['payload'] = JSON.parse(Base64.decode64(token.split('.')[1]))
+      decoded_token = {}
+      decoded_token['header'] = JSON.parse(Base64.decode64(token.split('.')[0]))
+      decoded_token['payload'] = JSON.parse(Base64.decode64(token.split('.')[1]))
 
-        user = JSON.parse(Cafmal::User.new(@cafmal_api_url, token).show(decoded_token['payload']['sub']))
-        team_id = user["team_id"]
-        email = user["email"]
+      user = JSON.parse(Cafmal::User.new(@cafmal_api_url, token).show(decoded_token['payload']['sub']))
+      team_id = user["team_id"]
+      email = user["email"]
 
-        #@TODO if you are the last logged in user, unsilence your team alerts
+      #@TODO if you are the last logged in user, unsilence your team alerts
 
-        # kind has to be login, as it's a label of events
-        event_id = JSON.parse(Cafmal::Event.new(@cafmal_api_url, token).create({name: 'user.logout', message: "#{email} has logged out.", kind: 'login', severity: 'info', team_id: team_id}))
+      # kind has to be login, as it's a label of events
+      event_id = JSON.parse(Cafmal::Event.new(@cafmal_api_url, token).create({name: 'user.logout', message: "#{email} has logged out.", kind: 'login', severity: 'info', team_id: team_id}))
 
-        if event_id.nil?
-          false
-        else
-          @token = nil
-          @decoded_token = nil
-          true
-        end
+      if event_id.nil?
+        false
+      else
+        @token = nil
+        @decoded_token = nil
+        true
       end
+    end
 
+    # refresh token
+    def refresh
+      headers = {"Content-Type" => "application/json", "Authorization" => "Bearer #{@token}"}
+      credentials = {token: @token}.to_json
+      refresh_request = Cafmal::Request::Post.new(@cafmal_api_url + '/user_token_refresh', credentials, headers)
+      if refresh_request.code < 300
+        @token = JSON.parse(refresh_request.response.body)['jwt']
+        @decoded_token = {}
+        @decoded_token['header'] = JSON.parse(Base64.decode64(@token.split('.')[0]))
+        @decoded_token['payload'] = JSON.parse(Base64.decode64(@token.split('.')[1]))
+
+        if (@decoded_token['payload']['role'] != 'worker' && @decoded_token['payload']['role'] != 'alerter')
+          team_id = JSON.parse(Cafmal::User.new(@cafmal_api_url, @token).show(@decoded_token['payload']['sub']))["team_id"]
+          event = Cafmal::Event.new(@cafmal_api_url, @token)
+          event.create({name: 'user.refresh_login', message: "#{@decoded_token['payload']['email']} has refreshed his login.", kind: 'login', severity: 'info', team_id: team_id})
+
+          #@TODO silence all alerts for your team_id, set silenced_at now + 1h
+        end
+        return true
+      else
+        return false
+      end
     end
   end
 end
